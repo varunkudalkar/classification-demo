d8a4ba6c7e06f9e7933ed27e724bb084e7f54092
varunkudalkar <varun.kudalkar@gmail.com>
2021-08-24 22:36:59 +0530

    code improvements

--- classify.py
+++ classify.py
@@ -1,56 +1,31 @@
-from os import wait
-import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
-from pandas.core.series import Series;
 
 mainDf = pd.read_excel('classifyVA_NVA.xlsx')
-nvaDf = pd.read_excel('NVA.xlsx')
+
 svaDf = pd.read_excel('SVA.xlsx')
 vaDf = pd.read_excel('VA.xlsx')
+nvaDf = pd.read_excel('NVA.xlsx')
 
 SVAfilteredDf = mainDf[mainDf.Category == 'SVA']
 VAfilteredDf = mainDf[mainDf.Category == 'VA']
 NVAAfilteredDf = mainDf[mainDf.Category == 'NVA']
 
-finalSVADf = DataFrame()
-for col in svaDf.columns:
-    for val in svaDf[col].to_list():
-        resDf = DataFrame()
-        if str(val) != 'nan':
-            resDf = SVAfilteredDf[SVAfilteredDf.Description.str.contains(val, case=False)]
-            resDf = resDf.assign(Classification=col)
-        if not resDf.empty:
-            finalSVADf = pd.concat([finalSVADf,resDf])
-            
-# print('SVA Dataframe: ')
-# print(finalSVADf)
+def sortDataFrames(colDf, filteredDf):
+    temDf = DataFrame()
+    for col in colDf.columns:
+        for val in colDf[col].to_list():
+            resDf = DataFrame()
+            if str(val) != 'nan':
+                resDf = filteredDf[filteredDf.Description.str.contains(val, case=False)]
+                resDf = resDf.assign(Classification=col)
+            if not resDf.empty:
+                temDf = pd.concat([temDf,resDf])
+    return temDf
 
-finalVADf = DataFrame()
-for col in vaDf.columns:
-    for val in vaDf[col].to_list():
-        resDf = DataFrame()
-        if str(val) != 'nan':
-            resDf = VAfilteredDf[VAfilteredDf.Description.str.contains(val, case=False)]
-            resDf = resDf.assign(Classification=col)        
-        if not resDf.empty:
-            finalVADf = pd.concat([finalVADf,resDf])
-
-# print('\n\nVA Dataframe: ')
-# print(finalVADf)
-
-finalNVADf = DataFrame()
-for col in nvaDf.columns:
-    for val in nvaDf[col].to_list():
-        resDf = DataFrame()
-        if str(val) != 'nan':
-            resDf = NVAAfilteredDf[NVAAfilteredDf.Description.str.contains(val, case=False)]
-            resDf = resDf.assign(Classification=col)        
-        if not resDf.empty:
-            finalNVADf = pd.concat([finalNVADf,resDf])
-
-# print('\n\nNVAA Dataframe: ')
-# print(finalNVADf)
+finalSVADf = sortDataFrames(svaDf, SVAfilteredDf)
+finalVADf = sortDataFrames(vaDf, VAfilteredDf)
+finalNVADf = sortDataFrames(nvaDf, NVAAfilteredDf)
 
 finalDf = DataFrame()
 finalDf = pd.concat([finalDf,finalSVADf])
@@ -62,6 +37,10 @@ finalDf = pd.concat([finalDf,mainDf])
 finalDf = finalDf[~finalDf.index.duplicated(keep='first')]
 finalDf = finalDf.sort_index(axis=0)
 
+print('Saving data to file')
+
 writer = pd.ExcelWriter("result.xlsx", engine='xlsxwriter')
 finalDf.to_excel(writer, index=False)
 writer.save()
+
+print('Saved data to file')
